cddef0f30454ef3cf47085d835baa8cf6c6c633b
dokumentS19 <dokument.o.s@ukr.net>
2026-05-27 21:46:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -225,8 +225,70 @@ const data = {
     info: ["Площа: 120 м²", "Ділянка: 5 соток", "Поверхів: 2"]
   },
   3: {
-    title: "1-к квартира, Ірпінь",
-    desc: "Компактна квартира в центрі міста.",
-function changeImg(el){
-  document.getElementById("main-img").src = el.src;
+  title: "1-к квартира, Ірпінь",
+  desc: "Компактна квартира в центрі міста.",
+  price: "38 000 $",
+  info: ["Площа: 38 м²", "Центр"]
 }
+
+};
+
+const images = {
+  1: [
+    "https://picsum.photos/800/400",
+    "https://picsum.photos/800/401",
+    "https://picsum.photos/800/402"
+  ],
+  2: [
+    "https://picsum.photos/800/410",
+    "https://picsum.photos/800/411",
+    "https://picsum.photos/800/412"
+  ],
+  3: [
+    "https://picsum.photos/800/420",
+    "https://picsum.photos/800/421",
+    "https://picsum.photos/800/422"
+  ]
+};
+
+  const obj = data[id];
+
+if (obj) {
+  document.getElementById("title").innerText = obj.title;
+  document.getElementById("desc").innerText = obj.desc;
+  document.getElementById("price").innerText = obj.price;
+
+  const ul = document.getElementById("info");
+  ul.innerHTML = "";
+
+  obj.info.forEach(i => {
+    const li = document.createElement("li");
+    li.innerText = i;
+    ul.appendChild(li);
+  });
+}
+
+const imgs = images[id];
+
+if (imgs) {
+  document.getElementById("main-img").src = imgs[0];
+
+  const thumbs = document.querySelector(".thumbs");
+
+  if (thumbs) {
+    thumbs.innerHTML = "";
+
+    imgs.forEach(src => {
+      const img = document.createElement("img");
+      img.src = src;
+
+      img.onclick = () => {
+        document.getElementById("main-img").src = src;
+      };
+
+      thumbs.appendChild(img);
+    });
+  }
+}
+``
+
