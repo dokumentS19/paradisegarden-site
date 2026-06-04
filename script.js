0a23b5c74e2fe143b59c1f95488e15bda611f155
dokumentS19 <dokument.o.s@ukr.net>
2026-06-04 15:56:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
- 
+   
 // ✅ 1. ІМПОРТИ (САМЕ НАВЕРХУ)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
