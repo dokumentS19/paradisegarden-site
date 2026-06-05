06d2e38bb1e0fde17ff2d7b64994060215f47ccf
dokumentS19 <dokument.o.s@ukr.net>
2026-06-05 18:37:31 +0300

    Update admin.js

--- assets/admin.js
+++ assets/admin.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
+  
 const firebaseConfig = {
   apiKey: "AIzaSy...",
   authDomain: "paradisegarden-site.firebaseapp.com",
