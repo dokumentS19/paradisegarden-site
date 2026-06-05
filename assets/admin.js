3d7cabee445b87b396e531bb5bd86157f3d1a974
dokumentS19 <dokument.o.s@ukr.net>
2026-06-05 18:31:59 +0300

    Update admin.js

--- assets/admin.js
+++ assets/admin.js
@@ -41,3 +41,43 @@ function toBase64(file) {
     reader.onload = () => resolve(reader.result);
   });
 }
+window.addObject = async () => {
+  const title = document.getElementById("title").value;
+  const area = Number(document.getElementById("area").value);
+  const price = Number(document.getElementById("price").value);
+
+  const files = document.getElementById("image").files;
+
+  const images = [];
+
+  for (let file of files) {
+    const base64 = await toBase64(file);
+    images.push(base64);
+  }
+
+  await addDoc(collection(db, "objects"), {
+    title,
+    area,
+    price,
+    images
+  });
+
+  alert("✅ Додано!");
+
+  // очистка полів ✅
+  document.getElementById("title").value = "";
+  document.getElementById("area").value = "";
+  document.getElementById("price").value = "";
+  document.getElementById("image").value = "";
+};
+
+// кнопка ✅
+document.getElementById("addBtn").onclick = window.addObject;
+
+function toBase64(file) {
+  return new Promise((resolve) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+  });
+}
