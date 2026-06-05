13920166c6fba47d8a3075e8834ce79116c607ad
dokumentS19 <dokument.o.s@ukr.net>
2026-06-05 18:30:15 +0300

    Create admin.js

---
+++ assets/admin.js
@@ -0,0 +1,43 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSy...",
+  authDomain: "paradisegarden-site.firebaseapp.com",
+  projectId: "paradisegarden-site"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+window.addObject = async () => {
+  const title = document.getElementById("title").value;
+  const area = Number(document.getElementById("area").value);
+  const price = Number(document.getElementById("price").value);
+
+  const files = document.getElementById("image").files;
+
+  const images = [];
+
+  for (let file of files) {
+    const base64 = await toBase64(file);
+    images.push(base64);
+  }
+
+  await addDoc(collection(db, "objects"), {
+    title,
+    area,
+    price,
+    images
+  });
+
+  alert("✅ Додано!");
+};
+
+function toBase64(file) {
+  return new Promise((resolve) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+  });
+}
