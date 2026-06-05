dc1eff2b29a18848b28a6535e403b7b407a67211
dokumentS19 <dokument.o.s@ukr.net>
2026-06-06 01:08:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let allObjects = [];
 import { initializeApp } 
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore, collection, getDocs, addDoc }
+import { getFirestore, collection, getDocs }
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 console.log("SCRIPT OK");
 const firebaseConfig = {
