5d93c578e88aa89a4b6dbe8e1df456a3e7933f5d
dokumentS19 <dokument.o.s@ukr.net>
2026-06-07 01:05:34 +0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,22 +1,53 @@
-import { db } from "./firebase.js";
+import { getStorage, ref, uploadBytes, getDownloadURL }
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
-import {
-  collection,
-  addDoc
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { initializeApp } 
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-document.getElementById("add").onclick = async () => {
+import { getFirestore, collection, addDoc } 
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-  const title = document.getElementById("title").value;
-  const area = document.getElementById("area").value;
-  const price = document.getElementById("price").value;
-
-  await addDoc(collection(db, "objects"), {
-    title,
-    area,
-    price
-  });
-
-  alert("✅ Додано");
+const firebaseConfig = {
+  apiKey: "AIzaSyB7Uu7Iq6X0471orSFgorzwwIqP5JMJeGk",
+  authDomain: "paradisegarden-site.firebaseapp.com",
+  projectId: "paradisegarden-site",
+ storageBucket: "paradisegarden-site.firebasestorage.app"
 };
 
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+document.getElementById("addBtn").onclick = async () => {
+
+  console.log("CLICK ✅");
+
+  const title = document.getElementById("title").value;
+  const area = Number(document.getElementById("area").value);
+  const price = Number(document.getElementById("price").value);
+  const files = document.getElementById("image").files;
+
+  const images = [];
+
+  try {
+    for (let file of files) {
+      const storageRef = ref(storage, "images/" + Date.now() + "_" + file.name);
+      const snapshot = await uploadBytes(storageRef, file);
+      const url = await getDownloadURL(snapshot.ref);
+      images.push(url);
+    }
+
+    await addDoc(collection(db, "objects"), {
+      title,
+      area,
+      price,
+      images
+    });
+
+    alert("✅ Додано!");
+
+  } catch (e) {
+    console.error(e);
+    alert("❌ Помилка");
+  }
+};
