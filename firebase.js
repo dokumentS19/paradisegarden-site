b3464d2db197a4039ea589d5ebe0031b8a271cf2
dokumentS19 <dokument.o.s@ukr.net>
2026-06-07 00:36:38 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,11 @@
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "ТУТ_API_KEY",
   authDomain: "paradisegarden-site.firebaseapp.com",
-  projectId: "paradisegarden-site",
-  storageBucket: "paradisegarden-site.firebasestorage.app"
+  projectId: "paradisegarden-site"
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const db = getFirestore(app);
-export const storage = getStorage(app);
-export const auth = getAuth(app);
