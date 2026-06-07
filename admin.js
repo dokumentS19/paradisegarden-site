11384c158fc34a914dd6ffd8f9d7e29f39bc82a3
dokumentS19 <dokument.o.s@ukr.net>
2026-06-07 05:06:07 +0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,6 +1,6 @@
 iimport { initializeApp }
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
+ 
 import { getFirestore, collection, addDoc }
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
