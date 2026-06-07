53f5ab85e570c3ab743eb46450b9579febd8406f
dokumentS19 <dokument.o.s@ukr.net>
2026-06-07 07:19:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -27,14 +27,15 @@ async function loadObjects() {
 
     const img = d.images?.[0] || d.image || "https://via.placeholder.com/400";
 
-    const html = `
-      <a href="object.html?id=${doc.id}" class="card">
-       <img src="${img}" alt="">
-        <h3>${d.title || "Без назви"}</h3>
-        <p>Площа: ${d.area || "-"} м²</p>
-        <strong>${d.price || "-"} $</strong>
-      </a>
-    `;
+   const html = `
+  <a href="object.html?id=${doc.id}" class="card">
+    <img src="${img}" alt="">
+
+    <h3>${d.title || "Без назви"}</h3>
+    <p>Площа: ${d.area || "-"} м²</p>
+    <strong>${d.price || "-"} $</strong>
+  </a>
+`;
 
     grid.insertAdjacentHTML("beforeend", html);
   });
