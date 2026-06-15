7838f1c9a0b555a2499274ee5d9cd5dfcde3c641
dokumentS19 <dokument.o.s@ukr.net>
2026-06-15 07:44:30 +0300

    Update favorites.js

--- favorites.js
+++ favorites.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
 import { 
-  getFirestore,
+  getFirestore, 
   doc,
   getDoc
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
