5e395060025a8883ba7f769888fbf344f6b9b3da
dokumentS19 <dokument.o.s@ukr.net>
2026-07-01 01:55:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-
 /* ================================
    STATE
 ================================ */
